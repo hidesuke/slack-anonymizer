8e3178e1a34997d4277e1a1b9c05be44950c7c9e
hidesuke <yusuke.hideshima@ghelia.com>
2019-05-21 11:31:33 +0900

    make thread anonymous

--- index.js
+++ index.js
@@ -2,24 +2,34 @@ const { WebClient } = require('@slack/web-api');
 
 const challenge = (payload, res) => res.status(200).json({ 'challenge': payload.challenge });
 
-const IGNORE_SUBTYPES = ['bot_message', 'channel_join', 'channel_leave', 'channel_topic', 'channel_purpose', 'file_share', 'pinned_item'];
+const IGNORE_SUBTYPES = ['bot_message', 'channel_join', 'channel_leave', 'channel_topic', 'channel_purpose', 'file_share', 'pinned_item', 'message_deleted', 'message_changed'];
+
+const deleteMessage = async (payload) => {
+  const client = new WebClient(process.env.SLACK_OAUTH_ACCESS_TOKEN);
+  await client.chat.delete({
+    channel: payload.event.channel,
+    ts: payload.event.ts,
+    as_user: true,
+  });
+};
+
+const postMessage = async (payload) => {
+  const client = new WebClient(process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN);
+  const options = {
+    channel: payload.event.channel,
+    text: payload.event.text,
+    icon_emoji: ':popuko:',
+    username: 'anonymous',
+  };
+  if (payload.event.thread_ts) options.thread_ts = payload.event.thread_ts;
+  await client.chat.postMessage(options);
+};
 
 const anonymize = async (payload, res) => {
   if (payload.event.channel !== process.env.HOOK_CHANNEL) return res.status(200).send('OK');
   try {
-    const deleteClient = new WebClient(process.env.SLACK_OAUTH_ACCESS_TOKEN);
-    await deleteClient.chat.delete({
-      channel: payload.event.channel,
-      ts: payload.event.ts,
-      as_user: true,
-    });
-    const postClient = new WebClient(process.env.SLACK_BOT_USER_OAUTH_ACCESS_TOKEN);
-    await postClient.chat.postMessage({
-      channel: payload.event.channel,
-      text: payload.event.text,
-      icon_emoji: ':popuko:',
-      username: 'anonymous',
-    });
+    await deleteMessage(payload);
+    await postMessage(payload);
   } catch (e) {
     console.log(JSON.stringify(e));
   }
